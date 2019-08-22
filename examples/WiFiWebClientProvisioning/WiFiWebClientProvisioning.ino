2434d4248347bc6d2c739bfc28e30f55eb07b05b
Martino Facchin <m.facchin@arduino.cc>
2019-08-22 18:36:01 +0200

    Add provisioning example

---
+++ examples/WiFiWebClientProvisioning/WiFiWebClientProvisioning.ino
@@ -0,0 +1,117 @@
+
+/*
+  Web client
+
+  This sketch connects to a website (http://www.google.com)
+  using the WiFi module.
+
+  This example is written for a network using WPA encryption. For
+  WEP or WPA, change the Wifi.begin() call accordingly.
+
+  This example is written for a network using WPA encryption. For
+  WEP or WPA, change the Wifi.begin() call accordingly.
+
+  Circuit:
+   Board with NINA module (Arduino MKR WiFi 1010, MKR VIDOR 4000 and UNO WiFi Rev.2)
+
+  created 13 July 2010
+  by dlf (Metodo2 srl)
+  modified 31 May 2012
+  by Tom Igoe
+*/
+
+#include <SPI.h>
+#include <WiFiNINA.h>
+
+char server[] = "www.arduino.cc";    // name address for Arduino.cc (using DNS)
+
+// Initialize the WiFi client library
+// with the IP address and port of the server
+// that you want to connect to (port 80 is default for HTTP):
+WiFiClient client;
+
+void setup() {
+  //Initialize serial and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  // check for the WiFi module:
+  if (WiFi.status() == WL_NO_MODULE) {
+    Serial.println("Communication with WiFi module failed!");
+    // don't continue
+    while (true);
+  }
+
+  String fv = WiFi.firmwareVersion();
+  if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
+    Serial.println("Please upgrade the firmware");
+  }
+
+  WiFi.beginProvision();
+  long start = millis();
+  bool displayHelpMessage = true;
+
+  while (WiFi.status() != WL_CONNECTED) {
+    if (millis() - start > 10000 && displayHelpMessage) {
+      Serial.println("Your board could be unprovisioned");
+      Serial.println("If so, connect to WiFiNINA network with your PC or smartphone");
+      Serial.println("If the browser doesn't redirect you, visit http://10.10.0.1");
+      // Just display the help message once
+      displayHelpMessage = false;
+    }
+    delay(1000);
+  }
+
+  Serial.println("Connected to wifi");
+  printWifiStatus();
+
+  Serial.println("\nStarting connection to server...");
+  // if you get a connection, report back via serial:
+  if (client.connect(server, 80)) {
+    Serial.println("connected to server");
+    // Make a HTTP request:
+    client.println("GET /asciilogo.txt HTTP/1.1");
+    client.println("Host: www.arduino.cc");
+    client.println("Connection: close");
+    client.println();
+  }
+}
+
+void loop() {
+  // if there are incoming bytes available
+  // from the server, read them and print them:
+  while (client.available()) {
+    char c = client.read();
+    Serial.write(c);
+  }
+
+  // if the server's disconnected, stop the client:
+  if (!client.connected()) {
+    Serial.println();
+    Serial.println("disconnecting from server.");
+    client.stop();
+
+    // do nothing forevermore:
+    while (true);
+  }
+}
+
+
+void printWifiStatus() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your board's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+}
